8a69c97fb39288ad1ffbbcd0a57dd661a2aaf0e4
luigi-ga <gallo.1895146@studenti.uniroma1.it>
2024-03-27 17:58:31 +0100

    Final commit

---
+++ code/src/rewards.py
@@ -0,0 +1,92 @@
+from typing import Dict, List, Tuple, Union, Any
+from sinergym.utils.rewards import LinearReward
+
+class FangerReward(LinearReward):
+
+    def __init__(
+        self,
+        temperature_variables: List[str],
+        energy_variables: List[str],
+        ppd_variable: Union[str, list],
+        occupancy_variable: Union[str, list],
+        range_comfort_winter: Tuple[int, int],
+        range_comfort_summer: Tuple[int, int],
+        summer_start: Tuple[int, int] = (6, 1),
+        summer_final: Tuple[int, int] = (9, 30),
+        energy_weight: float = 0.5,                         # ρ in the paper
+        lambda_energy: float = 1e-4,                        # λ_E in the paper
+        lambda_ppd: float = 0.1,                            # λ_P in the paper
+        lambda_temperature: float = 1.0
+    ):
+        """
+        Linear reward function using Fanger PPD thermal comfort.
+
+        It considers the energy consumption and the PPD thermal comfort metric, as well as occupancy.
+
+        .. math::
+            R = - W * lambda_E * power - (1 - W) * lambda_T * Fanger_metric * (occupancy > 0)
+
+        Args:       
+            temperature_variables (List[str]): Name(s) of the temperature variable(s).
+            energy_variables (List[str]): Name(s) of the energy/power variable(s).
+            ppd_variable (Union[str, list]): Name(s) of the PPD variable(s).
+            occupancy_variable (Union[str, list]): Name(s) of the occupancy variable(s).
+            range_comfort_winter (Tuple[int,int]): Temperature comfort range for cold season. Depends on environment you are using.
+            range_comfort_summer (Tuple[int,int]): Temperature comfort range for hot season. Depends on environment you are using.
+            summer_start (Tuple[int,int]): Summer session tuple with month and day start. Defaults to (6,1).
+            summer_final (Tuple[int,int]): Summer session tuple with month and day end. defaults to (9,30).
+            energy_weight (float, optional): Weight given to the energy term. Defaults to 0.5.
+            lambda_energy (float, optional): Constant for removing dimensions from power(1/W). Defaults to 1e-4.
+            lambda_ppd(float, optional): Constant for removing dimensions from ppd. Defaults to 0.1.
+            lambda_temperature (float, optional): Constant for removing dimensions from temperature(1/C). Defaults to 1.0.
+        """
+
+        # Call the parent constructor
+        super(FangerReward, self).__init__(
+            temperature_variables=temperature_variables,
+            energy_variables=energy_variables,
+            range_comfort_winter=range_comfort_winter,
+            range_comfort_summer=range_comfort_summer,
+            summer_start=summer_start,
+            summer_final=summer_final,
+            energy_weight=energy_weight,
+            lambda_energy=lambda_energy,
+            lambda_temperature=lambda_temperature
+        )
+
+        # Name of the variables
+        self.ppd_name = ppd_variable
+        self.occupancy_name = occupancy_variable
+
+        # Reward parameters
+        self.lambda_temp = lambda_ppd
+
+    def _get_comfort(self,
+                     obs_dict: Dict[str,
+                                    Any]) -> Tuple[float,
+                                                   List[float]]:
+        """Calculate the comfort term of the reward.
+        Returns:
+            Tuple[float, List[float]]: comfort penalty and List with temperatures used.
+        """
+
+        # Get the PPD and occupancy values
+        ppds = [v for k, v in obs_dict.items() if k in self.ppd_name]
+        occupancies = [v for k, v in obs_dict.items() if k in self.occupancy_name]
+
+        # Initialize comfort
+        comfort = 0.0
+
+        # Iterate over each zone
+        for ppd, occupancy in zip(ppds, occupancies):
+            # Calculate the comfort for each zone
+            zone_comfort = ppd * (occupancy > 0)
+            # If ppd < 20% it is within ASHRAE standards so it is not penalized
+            if zone_comfort >= 20:
+                comfort += zone_comfort
+
+        # Extract the temperature values
+        temp_values = [v for k, v in obs_dict.items() if k in self.temp_names]
+        
+        # Return the comfort and the temperature
+        return comfort, temp_values
---
+++ code/src/wrappers.py
@@ -0,0 +1,66 @@
+import json
+import sinergym
+import numpy as np
+import gymnasium as gym
+from src.modeling import CustomModelJSON
+from sinergym.utils.wrappers import NormalizeObservation
+
+class SinergymWrapper(gym.ObservationWrapper):
+    def __init__(
+            self,
+            env_config: dict
+        ):
+        # Create the environment
+        self.env = gym.make(
+            id=env_config['env_id'],
+            weather_files=env_config['weather_files'],
+            reward=env_config['reward'],
+            reward_kwargs=env_config['reward_kwargs'],
+            weather_variability=env_config['weather_variability'],
+        )     
+
+        # Modify the environment model with a custom one
+        # This part allows us to vary the 5 weather variables
+        self.env.unwrapped.model = CustomModelJSON(
+            env_name=env_config['env_id'],
+            json_file=self.env.unwrapped.building_file,
+            weather_files=self.env.unwrapped.weather_files,
+            actuators=self.env.unwrapped.actuators,
+            variables=self.env.unwrapped.variables,
+            meters=self.env.unwrapped.meters,
+            max_ep_store=self.env.unwrapped.model.max_ep_store,
+            extra_config=self.env.unwrapped.model.config,
+        )       
+        
+        # Normalize the observation space
+        self.env =  NormalizeObservation(self.env)
+
+        # Call the parent constructor
+        super(SinergymWrapper, self).__init__(self.env)
+
+    
+    def observation(self, observation):
+        return observation
+
+    def reset(self, seed=None):
+        #self.apply_new_weather_var()
+        obs, info = self.env.reset(seed=seed)
+        return obs
+
+    def step(self, action):
+        obs, reward, done, _, info = self.env.step(action)
+        return obs, reward, done, info
+
+    def close(self):
+        self.env.close()
+
+    def apply_new_weather_var(self, file_path='/home/luigi/Documents/SmartEnergyOptimizationRL/data/latest_env_config.json'):
+        try:
+            with open(file_path, 'r') as f:
+                weather_var = json.load(f)
+        except FileNotFoundError:
+            # Return a default configuration or raise an exception, depending on your preference
+            weather_var = {'drybulb': np.array([5.53173187e+00, 0.00000000e+00, 2.55034944e-03])}
+
+        # Apply new weather_variability
+        self.env.default_options['weather_variability'] = weather_var
